2dfa1af90be77d8641c37a241823309fd33577d6
iaaran <149783791+iaaran@users.noreply.github.com>
2024-03-02 18:20:05 +0800

    D19 Implement "Done todo item" feature

--- webtodo.py
+++ webtodo.py
@@ -2,21 +2,30 @@ import streamlit as st
 import functions as fn
 
 
+todos = fn.get_todos()
+
+
 def add_todo():
     new_todo = st.session_state['new_todo'] + "\n"
     todos.append(new_todo)
     fn.set_todos(todos)
+    st.session_state['new_todo'] = ""
 
-todos = fn.get_todos()
 
 st.title("My To-DO App")
 st.subheader(" This is my To-Do app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=f"todo{index}")
+    if checkbox:
+        st.write(f"Completed: {todos.pop(index)}")
+        fn.set_todos(todos)
+        del st.session_state[f"todo{index}"]
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo ...",
+
+st.text_input(label="Enter new todo", label_visibility='hidden', placeholder="Add new todo ...",
               on_change=add_todo, key='new_todo')
 
-st.session_state
+st.write(st.session_state)
