46ffb0f3534f30b91dea3740a93d29d998b38785
iaaran <149783791+iaaran@users.noreply.github.com>
2024-03-02 18:51:24 +0800

    D19 Refactor add and done item code

--- webtodo.py
+++ webtodo.py
@@ -6,10 +6,18 @@ todos = fn.get_todos()
 
 
 def add_todo():
-    new_todo = st.session_state['new_todo'] + "\n"
+    new_todo = st.session_state['new_todo'].strip().capitalize() + "\n"
+    if new_todo == "\n" or new_todo == " ":
+        return
     todos.append(new_todo)
     fn.set_todos(todos)
     st.session_state['new_todo'] = ""
+
+
+def done_todo(key):
+    index_local = int(key)
+    st.write(f"Completed: {todos.pop(index_local)}")
+    fn.set_todos(todos)
 
 
 st.title("My To-DO App")
@@ -17,15 +25,10 @@ st.subheader(" This is my To-Do app")
 st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=f"todo{index}")
-    if checkbox:
-        st.write(f"Completed: {todos.pop(index)}")
-        fn.set_todos(todos)
-        del st.session_state[f"todo{index}"]
-        st.experimental_rerun()
-
+    st.checkbox(todo, key=index, on_change=done_todo, args=(str(index)))
 
 st.text_input(label="Enter new todo", label_visibility='hidden', placeholder="Add new todo ...",
-              on_change=add_todo, key='new_todo')
+              on_change=add_todo, key='new_todo',
+              help='Type a new todo and press enter to add it to')
 
 st.write(st.session_state)
